0164567a9415e4d83bcbc606379ef18a539a7be5
sangwon <swchoi1997@naver.com>
2022-09-14 22:23:36 +0900

    hello servlet

--- src/main/java/hello/servlet/SpringMvcApplication.java
+++ src/main/java/hello/servlet/SpringMvcApplication.java
@@ -2,7 +2,9 @@ package hello.servlet;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.servlet.ServletComponentScan;
 
+@ServletComponentScan // spring이 servlet을 찾아서 등록해줌
 @SpringBootApplication
 public class SpringMvcApplication {
 
